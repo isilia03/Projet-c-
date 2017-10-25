cb5f17aede0fa561cd6b2dac204b71ad0dff4aff
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 09:09:28 +0200

    Create TextGestTopics.cs

---
+++ TextGestTopics.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class TextGestTopics : TopicsManager
+    {
+        public void CreateTopic(string topic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chatroom JoinTopic(string topic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ListTopic()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
