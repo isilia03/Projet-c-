a16fcfb218bb2ef2e40e79243b832c3e8531d609
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 10:59:22 +0200

    Delete AuthentificationManager.cs

--- AuthentificationManager.cs
+++
@@ -1,22 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chat.Authentification
-{
-    interface AuthentificationManager
-    {
-        //Ajouter un utilisateur
-        void AddUser(string login, string password);        //Supprimer un utilisateur        void RemoveUser(string login);
-        //Méthode d'authentification
-        void Authentify(string login, string password);
-
-        //Charger le fichier des users
-        AuthentificationManager Load(String path);
-
-        //Sauvegarde les fichiers des users
-        void Save(String path);
-    }
-}
