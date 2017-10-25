aa9fa28a5208ddd00f736a69fc2e458047a22db0
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 10:17:04 +0200

    Add files via upload

---
+++ Authentification/AuthentificationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Authentification
+{
+    class AuthentificationException : Exception
+    {
+        public string login;
+
+
+    }
+}
