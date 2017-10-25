04d5fcac7092a6673e7a8ecab44d77471045450d
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 09:21:41 +0200

    Update TextChatRoom.cs

--- TextChatRoom.cs
+++ TextChatRoom.cs
@@ -8,24 +8,39 @@ namespace Chat
 {
     class TextChatRoom : Chatroom
     {
+        string Topic;//string contenant le topic de la chatroom
+        List<Chatter> Chatters;//List contenant les chatters de la chatroom
+
+        //Constructeur
+        public TextChatRoom()
+        {
+            Chatters = new List<Chatter>();
+        }
+
+        //Récuperer le Topic de la chatroom
         public string getTopic()
         {
-            throw new NotImplementedException();
+            return Topic;
         }
 
+        //Rejoindre la chatroom
         public void Join(Chatter c)
         {
-            throw new NotImplementedException();
+            Chatters.Add(c);
+            Console.WriteLine("(Message from Chatroom : "+Topic+")"+ c.GetAlias() + "has join the room.");
         }
 
+        //Post un message
         public void Post(string msg, Chatter c)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("(At "+c.GetAlias()+") : "+c.GetAlias()+" $> " +msg);
         }
 
+        //Quitter la chatroom
         public void Quit(Chatter c)
         {
-            throw new NotImplementedException();
+            Chatters.Remove(c);
+            Console.WriteLine("(Message from Chatroom : " + Topic + ")" + c.GetAlias() + "has left the room.");
         }
     }
 }
