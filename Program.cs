cfb1050a8b6751fa9041be5322f374ede46179cb
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 11:43:24 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Chatprojet.Chat;
+﻿using Chat.Authentification;
+using Chatprojet.Chat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,9 +26,83 @@ namespace ChatProjet
             cr.Post("Je suis seul ou quoi ?", bob);
             cr.Join(joe);
             cr.Post("Tiens, salut Joe !", bob);
-            cr.Post("Toi aussi tu chat sur les forums de jeux pendant les TP,Bob ? ",joe);
+            cr.Post("Toi aussi tu chat sur les forums de jeux pendant les TP,Bob ? ", joe);
 
-            Console.ReadLine();
+            AuthentificationManager am = new Authentification();
+
+            // users management
+
+            try
+            {
+                am.AddUser("bob", "123");
+                Console.WriteLine("Bob has been added !");
+                am.RemoveUser("bob");
+                Console.WriteLine("Bob has been removed !");
+                am.RemoveUser("bob");
+                Console.WriteLine("Bob has been removes twice !");
+            }
+            catch (UserUnknownException e)
+            {
+                Console.WriteLine(e.Login + " : user unknown (enable to remove)!");
+            }
+            catch (UserExistsException e)
+            {
+                Console.WriteLine(e.Login + " has already been added !");
+            }
+
+            // authentification
+            try
+            {
+                am.AddUser("bob", "123");
+                Console.WriteLine("Bob has been added !");
+                am.authentify("bob", "123");
+                Console.WriteLine("Authentification OK !");
+                am.authentify("bob", "456");
+                Console.WriteLine("Invalid password !");
+            }
+            catch (WrongPassword e)
+            {
+                System.out.println(e.login + " has provided an invalid password !");
+            }
+            catch (UserExists e)
+            {
+                System.out.println(e.login + " has already been added !");
+
+            }
+            catch (UserUnknown e)
+            {
+                System.out.println(e.login + " : user unknown (enable to remove)
+               !");
+          }
+
+            // persistance
+            try
+            {
+                am.save("users.txt");
+                AuthentificationManager am1 = new Authentification();
+                am1.load("users.txt");
+                am1.authentify("bob", "123");
+                System.out.println("Loading complete !");
+            }
+            catch (UserUnknown e)
+            {
+                System.out.println(e.login + " is unknown ! error during the
+               saving / loading.");
+          }
+            catch (WrongPassword e)
+            {
+                System.out.println(e.login + " has provided an invalid password
+               !error during the saving / loading.");
+          }
+            catch (IOException e)
+            {
+                System.out.println(e);
+            }
+
         }
-    }
+
+
+        Console.ReadLine();
+        }
+}
 }
