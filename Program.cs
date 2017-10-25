f4a1dd3a3c8a949bb705c38e09ed2cf3ca6ad161
isilia03 <melo.1995@hotmail.fr>
2017-10-25 09:16:08 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Chatter bob = new TextChatter("Bob");
+            Chatter joe = new TextChatter("Joe");
+            TopicsManager gt = new TextGestTopics();
+            gt.createTopic("java");
+            gt.createTopic("UML");
+            gt.listTopics();
+            gt.createTopic("jeux");
+            gt.listTopics();
+            ChatRoom cr = gt.joinTopic("jeux");
+            cr.join(bob);
+            cr.post("Je suis seul ou quoi ?", bob);
+            cr.join(joe);
+            cr.post("Tiens, salut Joe !", bob);
+            cr.post("Toi aussi tu chat sur les forums de jeux pendant les TP,
+            Bob ? ",joe);*/
+        }
+    }
+}
