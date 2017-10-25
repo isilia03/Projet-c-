42ae6878cb1be79925d94ed4a89f10ca925eb4e4
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 10:57:05 +0200

    Add files via upload

---
+++ Authentification.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Authentification
+{
+    class Authentification : AuthentificationManager
+    {
+        List<User> users;//Liste d'utilisateurs
+
+        public Authentification()
+        {
+            users = new List<User>();
+        }
+
+        //Ajouter un utilisateur
+        public void AddUser(string login, string password)
+        {
+            users.Add(new User(login, password));
+        }
+
+        //Supprimer un utilisateur
+        public void RemoveUser(string login)
+        {
+            users.Remove(new User(login, ""));
+        }
+        //Méthode d'authentification
+        public void Authentify(string login, string password)
+        {
+            users.Contains(new User(login, password));
+        }
+
+        //Charger le fichier des users
+        public AuthentificationManager Load(String path)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Sauvegarde les fichiers des users
+        public void Save(String path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Authentification
+{
+    class User : IComparable
+    {
+        string Login { get; }
+        string _password;
+
+        public User(string login,string password)
+        {
+            Login = login;
+            _password = password;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            User otherUser = obj as User;
+            if (otherUser != null)
+            {
+                if (otherUser._login == this._login)
+                    return 0;
+                else
+                    return 1;
+            }
+            else
+                throw new ArgumentException("Object is not a User");
+        }
+    }
+}
