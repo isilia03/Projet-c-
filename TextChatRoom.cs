a906cb628fa5c1b5bb338f83577a7c21cbfce08f
TakeruGrima <timothee.le.corre@efrei.net>
2017-10-25 09:08:43 +0200

    Create TextChatRoom.cs

---
+++ TextChatRoom.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class TextChatRoom : Chatroom
+    {
+        public string getTopic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Join(Chatter c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post(string msg, Chatter c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Quit(Chatter c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
