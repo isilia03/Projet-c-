09980b9c0572ea50dea171d72c55c9d47e8a0d06
TakeruGrima <timothee.le.corre@efrei.net>
2017-11-15 08:37:07 +0100

    Update AuthentificationManager.cs

--- Authentification/AuthentificationManager.cs
+++ Authentification/AuthentificationManager.cs
@@ -9,12 +9,18 @@ namespace Chat.Authentification
     interface AuthentificationManager
     {
         //Ajouter un utilisateur
-        void AddUser(string login, string password);        //Supprimer un utilisateur        void RemoveUser(string login);
+        void AddUser(string login, string password);
+
+        //Supprimer un utilisateur
+        void RemoveUser(string login);
+
         //Méthode d'authentification
         void Authentify(string login, string password);
 
         //Charger le fichier des users
-        AuthentificationManager Load(String path);
+        //AuthentificationManager Load(String path);
+        /*On peut pas déclarer une méthode static dans l'interface nous avons donc mis en commentaire
+        cette méthode et implémenter la méthode static dans Authentification*/
 
         //Sauvegarde les fichiers des users
         void Save(String path);
